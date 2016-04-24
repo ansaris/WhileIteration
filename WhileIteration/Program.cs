eaae5f257b876cc4e29cf0ac8abe0e4cd203d225
ansaris <salman.ansari.usa@gmail.com>
2016-04-24 11:13:58 -0500

    Add MainMenu method.

--- WhileIteration/Program.cs
+++ WhileIteration/Program.cs
@@ -10,7 +10,36 @@ namespace WhileIteration
     {
         static void Main(string[] args)
         {
-            //test
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                Console.WriteLine("You selected Option 1");
+                Console.ReadKey();
+            }
+            else if (result == "2")
+            {
+                Console.WriteLine("You selected Option 2");
+                Console.ReadKey();
+            }
+            else if (result == "3")
+            {
+                Console.WriteLine("Goodbye.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("I didn't understand.");
+                Console.ReadKey();
+            }
         }
     }
 }
